8b1b24f72f9a3b66a9acfa75d19274b19dd8cf3b
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-08 16:31:46 -0500

    creacion de ruta para cars e implementacion de parametros en la rutas bajo el uso de los decoradores

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('cars')
-export class CarsController {}
+export class CarsController {
+  constructor() {}
+  @Get()
+  getAllCars() {
+    return ['Toyota', 'Honda', 'Jeep'];
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id) {
+    console.log({ id });
+
+    return { id };
+  }
+
+  @Post()
+  createCar() {
+    return ['create Car!!'];
+  }
+
+  @Put()
+  updateCar() {
+    return ['update Car!'];
+  }
+}
