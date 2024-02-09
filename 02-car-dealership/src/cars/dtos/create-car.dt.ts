b0352e5d49dbb32967841d4c36b1013fe16123c4
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-09 12:06:47 -0500

    uso del decorador para personalizar el mensaje de error dentro del dto create, tener en cuenta que tipo de decorador involucrar

--- 02-car-dealership/src/cars/dtos/create-car.dt.ts
+++ 02-car-dealership/src/cars/dtos/create-car.dt.ts
@@ -1,7 +1,7 @@
 import { IsString } from 'class-validator';
 
 export class CreateCarDto {
-  @IsString()
+  @IsString({ message: `brand is required` })
   readonly brand: string;
   @IsString()
   readonly model: string;
