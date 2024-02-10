a0b755d77be17aee6ce8a7c4c584bf6b8f5d729d
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-10 09:59:04 -0500

    especificacion de esquema

--- 03-pokedex/src/pokemon/entities/pokemon.entity.ts
+++ 03-pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,10 @@
-export class Pokemon {}
+import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  name: string;
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
