2b12c2de09873fdd38e942498068bef94147933d
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-11 12:50:07 -0500

    implementacion de joi para manejar el schema para la validacion de las variables de entorno

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -4,16 +4,18 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 // import { Mongoose } from 'mongoose';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/envs.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [EnvConfiguration],
+      load: [EnvConfiguration], //coversiones
+      validationSchema: JoiValidationSchema, //validacion
     }), // colocar siempre al inicio para las envs
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
--- 03-pokedex/src/config/envs.config.ts
+++ 03-pokedex/src/config/envs.config.ts
@@ -2,5 +2,5 @@ export const EnvConfiguration = () => ({
   environment: process.env.NODE_ENV || 'dev',
   mongodb: process.env.MONGO_DB,
   port: process.env.PORT || 3001,
-  defaultLimit: process.env.DEFAULT_LIMIT || 7,
+  defaultLimit: +process.env.DEFAULT_LIMIT || 7,
 });
