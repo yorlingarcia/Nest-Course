8c270ae64ba92090f3caed7aa0a5e79e6009a8b5
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-18 17:10:36 -0500

    creacion de tags en la entidad

--- 04-teslo-shop/src/products/entities/product.entity.ts
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -45,6 +45,13 @@ export class Product {
   @Column('text')
   gender: string;
 
+  @Column({
+    type: 'text',
+    array: true,
+    default: [],
+  })
+  tags: string[];
+
   @BeforeInsert()
   checkSlugInsert() {
     if (!this.slug) {
