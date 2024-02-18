d2cb5b46687ddc71942be1c538552b7cc629ad11
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-18 17:44:08 -0500

    creacion de relacion en postgres

--- 04-teslo-shop/src/products/entities/product.entity.ts
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -3,8 +3,10 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { ProductImage } from './product-image.entity';
 
 @Entity()
 export class Product {
@@ -51,6 +53,11 @@ export class Product {
     default: [],
   })
   tags: string[];
+
+  @OneToMany(() => ProductImage, (productImage) => productImage.product, {
+    cascade: true,
+  })
+  images?: ProductImage[];
 
   @BeforeInsert()
   checkSlugInsert() {
