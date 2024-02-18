bc3cca7c10b7311dc58ed3bd9616a6e7e7001dd7
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-18 17:28:28 -0500

    creacion de archivo de barril y actulaizacion de importaciones

--- 04-teslo-shop/src/products/products.module.ts
+++ 04-teslo-shop/src/products/products.module.ts
@@ -2,8 +2,7 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
-import { ProductImage } from './entities/product-image.entity';
+import { Product, ProductImage } from './entities';
 
 @Module({
   controllers: [ProductsController],
