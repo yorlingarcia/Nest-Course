78ca73aac42d4cc77fc70e70d0506fa034515256
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-18 14:27:11 -0500

    creacion de logica para la obtencion y eliminacion de productos

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -28,20 +29,27 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return this.productRepository.find();
+  //Todo: Paginar
+  async findAll() {
+    const products = await this.productRepository.find({});
+    return products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product)
+      throw new NotFoundException(`Product with id ${id} not found`);
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.delete({ id });
+    return product;
   }
 
   private handleDbExceptions(error: any) {
