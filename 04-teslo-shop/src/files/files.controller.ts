87b8bf36bc84e5012883bcb344601764033ae727
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-20 12:19:35 -0500

    creacion de rest api par carga de archivos

---
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { CreateFileDto } from './dto/create-file.dto';
+import { UpdateFileDto } from './dto/update-file.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    return file;
+  }
+}
