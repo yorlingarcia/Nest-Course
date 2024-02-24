3dac7bf6bf8615a6a7c28b80272c74c1d3389330
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-23 22:54:16 -0500

    lectura de coneccion en el cliente

--- 05-ws-client/src/main.ts
+++ 05-ws-client/src/main.ts
@@ -4,7 +4,7 @@ import "./style.css";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Websocket - Client</h1>
-    <span>Offline</span>
+    <span id="server-status">Offline</span>
   </div>
 `;
 connectToServer();
--- 05-ws-client/src/socket-client.ts
+++ 05-ws-client/src/socket-client.ts
@@ -1,7 +1,20 @@
-import { Manager } from "socket.io-client";
+import { Manager, Socket } from "socket.io-client";
 
 export const connectToServer = () => {
   const manager = new Manager("http://localhost:3000/socket.io/socket.io.js");
 
   const socket = manager.socket("/");
+  addListeners(socket);
+};
+
+const addListeners = (socket: Socket) => {
+  const serverStatusLabel = document.querySelector("#server-status");
+
+  socket.on("connect", () => {
+    serverStatusLabel!.innerHTML = "Connected";
+  });
+
+  socket.on("disconnect", () => {
+    serverStatusLabel!.innerHTML = "Disconnected";
+  });
 };
