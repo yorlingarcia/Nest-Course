31d021d28d945bdb4eef105d42ad5677fdcb22ab
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-23 22:16:13 -0500

    implementacion de seocket.io

--- 04-teslo-shop/src/messages-ws/messages-ws.gateway.ts
+++ 04-teslo-shop/src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,20 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+@WebSocketGateway({ cors: true, namespace: '' })
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messagesWsService: MessagesWsService) {}
+  handleConnection(client: Socket) {
+    console.log('Client conected!', client.id);
+  }
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected!', client.id);
+  }
 }
